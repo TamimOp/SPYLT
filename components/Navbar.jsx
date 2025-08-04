a027ceda11bae5b70df5ea4c3ea589ef65ed63da
TamimOp <tamimshadanik@gmail.com>
2025-08-04 13:17:25 +0600

    design the navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="fixed top-0 left-0 z-50 md:p-9 p-3">
+      <Image
+        src="/images/nav-logo.svg"
+        alt="nav-logo"
+        width={96}
+        height={96}
+        className="md:w-24 w-20"
+      />
+    </nav>
+  );
 };
 
 export default Navbar;
