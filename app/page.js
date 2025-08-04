2b59c4b653b4bc45fa656b9d8b5219165c10234c
TamimOp <tamimshadanik@gmail.com>
2025-08-04 15:43:43 +0600

    Add HeroSection and MessageSection components to the main page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
-import HeroSection from "@/components/HeroSection";
+import HeroSection from "@/sections/HeroSection";
 import Navbar from "@/components/Navbar";
 import React from "react";
+import MessageSection from "@/sections/MessageSection";
 
 const page = () => {
   return (
     <main>
       <Navbar />
       <HeroSection />
+      <MessageSection />
     </main>
   );
 };
